b86ecd4d6349d45f6a8490c9fde83a032419fe24
李明飞 <limingfei@limingfeideMacBook-Pro.local>
2020-12-16 23:03:11 +0800

    post

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const multer = require('multer')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var signupRouter = require('./routes/signup')
+var signupRouter = require('./routes/signup');
+var postsRouter =  require('./routes/posts')
 
 var app = express();
 
@@ -57,6 +58,7 @@ app.use(function(req, res, next) {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
